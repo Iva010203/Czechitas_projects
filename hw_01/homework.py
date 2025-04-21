8cb9009c921e97c6d47d13e21d1610c017d9c414
Iva010203 <iva.jankovcova@seznam.cz>
2025-04-21 13:12:00 +0200

    inital draft

---
+++ hw_01/homework.py
@@ -0,0 +1,11 @@
+# 1. nacist soubor Alice.txt (po radcich)
+# 2. odstranit mezery a konce radku, prevezt velka pismena na mala
+# 3. napsat skript na spocitani cetnosti jednotlivyvch znaku
+# 4. seradit dle klicu (klic = pismena)
+# 5. seradit dle cetnosti vyskytu
+# 6. ulozit do JSON souboru hw01_output.json
+# 7. dat na google drive ci git
+
+
+
+
