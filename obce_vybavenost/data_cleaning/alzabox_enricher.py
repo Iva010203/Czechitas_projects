fa4fab05240dec23927bfd74cf6ce87f9c043f01
Iva Prochazkova <iva.jankovcova@seznam.cz>
2025-05-04 11:07:34 +0200

    Refactor alzabox data enrichment script to improve clarity and remove unused code

--- obce_vybavenost/data_cleaning/alzabox_enricher.py
+++ obce_vybavenost/data_cleaning/alzabox_enricher.py
@@ -1,9 +1,7 @@
 """
 this script is used to enrich the alzabox data with adddress data from the adresy_cr dataset
-this is particularly useful to enrich alzabox data with city_code from adresy_cr dataset
 the script reads the cleaned alzabox dataset (data/clean/alzaboxes_cleaned.csv) and the cleaned adresy_cr dataset (data/clean/adresy_cr/combined_adresy_cr_cleaned.csv)
-it then merges the two datasets on the street and number columns
-the script also adds the city_code from the adresy_cr dataset to the alzabox dataset
+it then merges the two datasets on the city and postal code columns
 """
 
 import pandas as pd
@@ -20,37 +18,26 @@ def enrich_alzabox_data(alzabox_file, adresy_file, output_file):
     addresses_df = addresses_df[relevant_columns]
     
     # Apply case insensitive merge
-    # alzabox_df['street_lower'] = alzabox_df['street'].str.lower()
-    # addresses_df['street_lower'] = addresses_df['street'].str.lower()
+
     alzabox_df['city_lower'] = alzabox_df['city'].str.lower()
     addresses_df['city_lower'] = addresses_df['city'].str.lower()
 
     # remove city part number if exists; e.f. "Praha 1" -> "Praha"    
     alzabox_df['city_lower'] = alzabox_df['city_lower'].str.replace(r'\s\d+', '', regex=True)
 
-    # fill in empty values for street_lower
-    # alzabox_df['street_lower'] = alzabox_df['street_lower'].fillna('')
-    # addresses_df['street_lower'] = addresses_df['street_lower'].fillna('')
-    
-    # Merge the two datasets on the street and city columns
-    # Note: This would produce duplicate rows
+    # Merge the two datasets on the city and postal_code columns
     enriched_df = pd.merge(alzabox_df, addresses_df, on=['city_lower', 'postal_code'], how='left')
 
     # Drop the temporary columns used for merging
-    # enriched_df = enriched_df.drop(columns=['street_lower', 'city_lower'])
     enriched_df = enriched_df.drop(columns=['city_lower',])
 
     # Drop duplicates if any
     enriched_df = enriched_df.drop_duplicates()
 
-    # Add the city_code from the adresy_cr dataset to the alzabox dataset
-    # enriched_df['city_code'] = enriched_df['city_code']
-
     # print number of rows with missing addresses_df and those with matching addresses_df
     missing_addresses = enriched_df[enriched_df['city_code'].isnull()]
-    print(f"Number of rows with missing addresses_df: {len(missing_addresses)}")
-    print(f"Number of rows with matching addresses_df: {len(enriched_df) - len(missing_addresses)}")
-    # print number of rows with missing alzabox_df and those with matching alzabox_df
+    overall_count = len(enriched_df)    
+    print(f"Number of rows with missing addresses_df: {len(missing_addresses)} out of {overall_count}, which is {len(missing_addresses)/overall_count:.2%}")
 
     # Save the enriched dataset
     enriched_df.to_csv(output_file, index=False)
